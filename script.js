c621fde7075216a373da5a08eb235c0eb16b837c
Arjun Shukla-Degen <arjunshukladegen@gmail.com>
2026-07-21 13:46:57 -0700

    Set up the basic javascript for the 0 button

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ const screenValue = document.querySelector("#screen");
 
 // Render calculations to screen
 let currentScreen = "";
-let screenArgs = {};
+let argCount = 0;
 let currentValue = 0;
 
-function renderScreen(currentScreen) {
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function addValue(arg) {
+  if (numbers.includes(arg)) {
+    currentScreen = currentScreen + arg;
+    currentValue = currentValue + arg;
+  }
+}
+
+function renderScreen() {
   screenValue.value = currentScreen;
 }
 
@@ -36,3 +45,8 @@ const Button = {
   clear: document.querySelector("#clear"),
   backspace: document.querySelector("#backspace"),
 };
+
+Button.zero.addEventListener("click", function (e) {
+  addValue(0);
+  renderScreen();
+});
